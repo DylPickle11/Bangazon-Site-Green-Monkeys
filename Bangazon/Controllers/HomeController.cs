3140faaaef0b3a1937d0685bad7f855154db27d9
Phil Griswold <philgriswold@me.com>
2020-01-27 09:21:48 -0800

    first commit

--- Bangazon/Controllers/HomeController.cs
+++ Bangazon/Controllers/HomeController.cs
@@ -5,14 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Bangazon.Models;
+using Bangazon.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Bangazon.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index(string searchQuery)
+        {
+            if (searchQuery == null)
+            {
+                return View(await _context.Product
+                       .ToListAsync());
+            }
+            else
+            {
+                //searchQuery = searchQuery;
+                return View(await _context.Product
+              .Where(p => p.Title.Equals(searchQuery))
+                    .ToListAsync());
+            }
         }
 
         public IActionResult Privacy()
@@ -25,5 +46,6 @@ namespace Bangazon.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
     }
 }
